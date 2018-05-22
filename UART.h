0f2c16c383f7ca638a94986184e1324112a0b6e1
Akshay <akshay41298@gmail.com>
2018-05-22 15:34:28 +0530

    Modified UART.h for negative integers

--- UART.h
+++ UART.h
@@ -50,50 +50,104 @@ unsigned char UartReceive(void)
 }
 
 void UartTransmit(int a){
-	int size=0,hex=0;
-	int x=a;
-	while(a!=0){
-		a=a/10;
-		size++;
-	}
-	int arr[size];
+	if(a>=0){
+		int size=0,hex=0;
+		int x=a;
+		while(a!=0){
+			a=a/10;
+			size++;
+		}
+		int arr[size];
 
-	for(int i=size-1;i>=0;i--){
-		arr[i]=x%10;
-		x=x/10;
-	}
-	for(int i=0;i<size;i++){
-		hex=arr[i]+48;
-		USART2 ->DR = hex;
-		while(!(USART2->SR & (USART_SR_TXE)));
+		for(int i=size-1;i>=0;i--){
+			arr[i]=x%10;
+			x=x/10;
+		}
+		for(int i=0;i<size;i++){
+			hex=arr[i]+48;
+			USART2 ->DR = hex;
+			while(!(USART2->SR & (USART_SR_TXE)));
 
+			delay(1000);
+		}
+	}
+	else{
+		a *= -1;
+		USART2 ->DR = 0x2D;
 		delay(1000);
+		int size=0,hex=0;
+		int x=a;
+		while(a!=0){
+			a=a/10;
+			size++;
+		}
+		int arr[size];
+
+		for(int i=size-1;i>=0;i--){
+			arr[i]=x%10;
+			x=x/10;
+		}
+		for(int i=0;i<size;i++){
+			hex=arr[i]+48;
+			USART2 ->DR = hex;
+			while(!(USART2->SR & (USART_SR_TXE)));
+
+			delay(1000);
+		}
 	}
 
 
 }
 void UartTransmitln(int a){
-	int size=0,hex=0;
-	int x=a;
-	while(a!=0){
-		a=a/10;
-		size++;
-	}
-	int arr[size];
+	if(a>=0){
+		int size=0,hex=0;
+		int x=a;
+		while(a!=0){
+			a=a/10;
+			size++;
+		}
+		int arr[size];
 
-	for(int i=size-1;i>=0;i--){
-		arr[i]=x%10;
-		x=x/10;
-	}
-	for(int i=0;i<size;i++){
-		hex=arr[i]+48;
-		USART2 ->DR = hex;
+		for(int i=size-1;i>=0;i--){
+			arr[i]=x%10;
+			x=x/10;
+		}
+		for(int i=0;i<size;i++){
+			hex=arr[i]+48;
+			USART2 ->DR = hex;
+			while(!(USART2->SR & (USART_SR_TXE)));
+
+			delay(1000);
+		}
+		USART2 -> DR=0x0A;
 		while(!(USART2->SR & (USART_SR_TXE)));
-
-		delay(1000);
 	}
-	USART2 -> DR=0x0A;
-	while(!(USART2->SR & (USART_SR_TXE)));
+	else{
+		a *= -1;
+		USART2 ->DR = 0x2D;
+		delay(1000);
+		int size=0,hex=0;
+		int x=a;
+		while(a!=0){
+			a=a/10;
+			size++;
+		}
+		int arr[size];
+
+		for(int i=size-1;i>=0;i--){
+			arr[i]=x%10;
+			x=x/10;
+		}
+		for(int i=0;i<size;i++){
+			hex=arr[i]+48;
+			USART2 ->DR = hex;
+			while(!(USART2->SR & (USART_SR_TXE)));
+
+			delay(1000);
+		}
+		USART2 -> DR=0x0A;
+		while(!(USART2->SR & (USART_SR_TXE)));
+	}
 
 }
 
